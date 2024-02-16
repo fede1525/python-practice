8de3cba850dae35a627c708229fab03cc339357f
Federico Resano <federicoresano1@gmail.com>
2024-02-16 17:38:29 -0300

    commit

--- exercises/008-two_timestamps/app.py
+++ exercises/008-two_timestamps/app.py
@@ -1,7 +1,13 @@
 def two_timestamp(hr1, min1, sec1, hr2, min2, sec2):
-    # Your code here   
-    return None
+    total_hr = hr2 - hr1 
+    total_min = min2 - min1
+    total_sec = sec2 - sec1
+
+    hr_to_sec = total_hr * 3600
+    min_to_sec = total_min * 60 
+
+    result = hr_to_sec + min_to_sec + total_sec
+    return result
 
 
-# Invoke the function and pass two timestamps(6 intergers) as its arguments
 print(two_timestamp(1,1,1,2,2,2))
