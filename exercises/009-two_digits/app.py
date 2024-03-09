c36090ef5639fa3577b3337938196cb0c1be5d58
fede <federicoresano1@gmail.com>
2024-03-09 11:35:55 -0300

    exercises 9 and 11

--- exercises/009-two_digits/app.py
+++ exercises/009-two_digits/app.py
@@ -1,6 +1,8 @@
 # Complete the function to return the tens digit and the units digit of any interger
 def two_digits(number):
-  return None
+    decimals = number / 10
+    units = number % 10
+    return decimals, units 
    
 
 # Invoke the function with any two digit integer as its argument
--- exercises/011-last_two_digits/app.py
+++ exercises/011-last_two_digits/app.py
@@ -1,6 +1,11 @@
 # Complete the function to print the last two digits of an integer greater than 9
 def last_two_digits(num):
-    return None
+    first_digit = 0
+    second_digit = 0
+    if num > 9:
+        first_digit = num / 10
+        second_digit = num % 10
+    return first_digit, second_digit
 
 # Invoke the function with any integer greater than 9
-print(last_two_digits())
+print(last_two_digits(76))
