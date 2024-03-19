f8dd03d3543edc0a148b2b8be672a2cb9fea0d83
fede <federicoresano1@gmail.com>
2024-03-19 20:41:37 -0300

    Exercise 35

--- exercises/034-a_aa_aaa_aaaa/app.py
+++ exercises/034-a_aa_aaa_aaaa/app.py
@@ -1,1 +1,6 @@
-# Your code here
+def computed_value(a):
+   a_str = str(a)
+
+   return a + int(a_str*2) + int(a_str * 3) + int(a_str *4)
+
+print(computed_value(9))
--- exercises/035-square-each-odd-number/app.py
+++ exercises/035-square-each-odd-number/app.py
@@ -1,1 +1,4 @@
-# Your code here
+def square_odd_numbers(string):
+    return [int(num) ** 2 for num in string.split(",") if int(num) % 2 != 0]
+
+print(square_odd_numbers("1,2,3,4,5,6,7,8,9"))
